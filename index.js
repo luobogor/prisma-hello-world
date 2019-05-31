748195becc0c7434daac8f2b8aa1b2261b300e51
“jinzhanye” <jinzhan.ye@foxmail.com>
2019-05-31 17:30:46 +0800

    simple demo 02

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-const { prisma } = require('./generated/prisma-client');
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
 
-// 一个main函数，以便我们可以使用async/await
-async function main() {
-// 新建一个user，并新建一个post文章
-  const newUser = await prisma
-    .createUser({
-      name: "Alice",
-      posts: {
-        create: {
-          title: "The data layer for modern apps",
-        }
-      },
-    });
-  console.log("Created new user: ${newUser.name} (ID: ${newUser.id})");
-  // 从数据库中读取所有用户user并将其打印到控制台
-  const allUsers = await prisma.users();
-  console.log(allUsers);
-  // 从数据库中读取所有文章post并将其打印到控制台
-  const allPosts = await prisma.posts();
-  console.log(allPosts);
+const resolvers = {
+  Query: {
+    publishedPosts(root, args, context) {
+      return context.prisma.posts({ where: { published: true } })
+    },
+    post(root, args, context) {
+      return context.prisma.post({ id: args.postId })
+    },
+    postsByUser(root, args, context) {
+      return context.prisma.user({
+        id: args.userId
+      }).posts()
+    }
+  },
+  Mutation: {
+    createDraft(root, args, context) {
+      return context.prisma.createPost(
+        {
+          title: args.title,
+          author: {
+            connect: { id: args.userId }
+          }
+        },
+      )
+    },
+    publish(root, args, context) {
+      return context.prisma.updatePost(
+        {
+          where: { id: args.postId },
+          data: { published: true },
+        },
+      )
+    },
+    createUser(root, args, context) {
+      return context.prisma.createUser(
+        { name: args.name },
+      )
+    }
+  },
+  User: {
+    posts(root, args, context) {
+      return context.prisma.user({
+        id: root.id
+      }).posts()
+    }
+  },
+  Post: {
+    author(root, args, context) {
+      return context.prisma.post({
+        id: root.id
+      }).author()
+    }
+  }
 }
 
-main().catch(e => console.error(e));
+
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
